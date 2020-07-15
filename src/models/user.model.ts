e025331c8ef1947451347b6e0cd05641a2553fab
Daniel Tian <dtian.95@gmail.com>
2020-07-15 14:48:10 +0800

    login is partially done, need to generate jwt token

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,6 +2,14 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
+interface IUser extends mongoose.Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  refreshToken: string;
+}
+
 const UserSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
@@ -10,6 +18,6 @@ const UserSchema = new Schema({
   refreshToken: { type: String, required: false },
 });
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model<IUser>("User", UserSchema);
 
 export { User };
--- src/routes/auth.api.ts
+++ src/routes/auth.api.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { User } from "../models/user.model";
 import { errorHandler } from "./error";
 import { hash, compare } from "bcrypt";
-import { verify } from "jsonwebtoken";
+import "../utils/config";
 
 const router = express.Router();
 
@@ -10,21 +10,19 @@ const saltRounds = 10;
 
 // create new user
 router.post("/signup", async (req, res) => {
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
-  const password = req.body.password;
+  const firstName = req.body.firstName,
+    lastName = req.body.lastName,
+    email = req.body.email,
+    password = req.body.password;
 
   if (await User.findOne({ email: email })) {
-    return res
-      .status(400)
-      .json({ success: false, messsage: "User already exists." });
+    return errorHandler(res, "User already exists.");
   }
 
   // hash + salt password
   hash(password, saltRounds, (err, hash) => {
     if (err) {
-      return errorHandler(res, err);
+      return errorHandler(res, err.message);
     }
 
     const newUser = new User({
@@ -36,7 +34,7 @@ router.post("/signup", async (req, res) => {
 
     newUser
       .save()
-      .then((data) => {
+      .then(() => {
         return res.status(200).json({ success: true });
       })
       .catch((err) => {
@@ -45,12 +43,37 @@ router.post("/signup", async (req, res) => {
   });
 });
 
+// login user
+router.post("/login", async (req, res) => {
+  const email = req.body.email,
+    password = req.body.password;
+
+  const JWT_SECRET = process.env.JWT_SECRET;
+
+  User.findOne({ email: email }).then((user) => {
+    // user does not exist
+    if (!user) return errorHandler(res, "User email or password is incorrect.");
+
+    compare(password, user.password, (err, result) => {
+      if (err) return errorHandler(res, err.message);
+
+      if (result == true) {
+        // password matched
+        return res.status(200).json({ success: true });
+      } else {
+        // wrong password
+        return errorHandler(res, "User email or password is incorrect.");
+      }
+    });
+  });
+});
+
 // TESTING ROUTES BELOW
 // get all users
 router.get("/", (_, res) => {
   User.find({})
     .then((result) => {
-      return res.status(200).json({ message: "success", result: result });
+      return res.status(200).json({ success: true, result: result });
     })
     .catch((e) => {
       return errorHandler(res, e);
@@ -61,7 +84,7 @@ router.get("/", (_, res) => {
 router.delete("/", (_, res) => {
   User.deleteMany({})
     .then(() => {
-      return res.status(200).json({ message: "success" });
+      return res.status(200).json({ success: true });
     })
     .catch((e) => {
       return errorHandler(res, e);
