0517be781de6583a76add15749c8d5cb7b810d77
kayleegeorge <62825936+kayleegeorge@users.noreply.github.com>
2020-07-17 13:57:49 -0700

    Update Signup.tsx

--- src/client/src/pages/Signup.tsx
+++ src/client/src/pages/Signup.tsx
@@ -1,12 +1,54 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { Button } from 'semantic-ui-react';
 
 const Headline = styled.h1`
     padding-top: 7%;
-    font-family: Roboto;
-    font-size: 30px;
+    font-family: 'Avenir';
+    font-size: 33px;
     text-align: center;
-    color: black;
+    color: white;
+    padding-bottom: 10px;
+`;
+
+const NavBar = styled.div`
+  max-width: 80%;
+  margin: auto;
+  margin-top: 34px;
+  display: flex;
+`;
+
+const Background = styled.div`
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  min-width: 100%;
+  min-height: 100%;
+  opacity: 100%;
+  background-color: #ffa3a3;
+`;
+
+const ButtonContainer = styled.div`
+  display:flex;
+  width:400px;
+  margin-left:46%
+`;
+
+const Logo = styled.div`
+  font-size: 30px;
+`;
+
+const PigeonName = styled.div`
+  font-size: 30px;
+  margin-left: 10px;
+  font-family: 'Avenir';
+  font-weight: 500;
+  color: white;
+`;
+
+
+const Body = styled.div`
+  background-color: #ffa3a3;
 `;
 
 // TODO: Decide on Formik or another way of processing form submissions
@@ -42,30 +84,27 @@ const InputField = styled.input`
     }
 `;
 
-const SubmitButton = styled.button`
-    font-family: 'Roboto', sans-serif, bold;
-    font-size: 18px;
-    border-color: pink;
-    background-color: #ffb9b9;
-    padding: 10px;
-    border-radius: 10px;
-    color: white;
-    width: 350px;
-    margin: auto;
-`;
 
 const Signup = () => {
     return (
-        <>
+        <Background>
+        <NavBar>
+          <Logo>🐦</Logo>
+          <PigeonName>Pigeon</PigeonName>
+        </NavBar>
+          <Body>
             <Headline>Pigeon Sign Up</Headline>
             <FormContainer>
-                <InputField type="text" name="name" placeholder="Jane Doe" />
                 <InputField type="text" name="email" placeholder="janedoe@gmail.com" />
-                <InputField type="password" name="password" placeholder="1234" />
-                <InputField type="text" name="confirm-password" placeholder="1234" />
-                <SubmitButton type="submit">Get started!</SubmitButton>
+                <InputField type="text" name="name" placeholder="Jane Doe" />
+                <InputField type="password" name="password" placeholder="Password" />
+                <InputField type="text" name="confirm-password" placeholder="Confirm Password" />
+                <ButtonContainer>
+                  <button className="ui primary button">Get Started</button>
+                </ButtonContainer>
             </FormContainer>
-        </>
+          </Body>
+        </Background>
     );
 };
 
