cb675029e5299a2b48b2dab670439ce6306916c2
Daniel Tian <dtian.95@gmail.com>
2020-07-19 17:31:19 +0800

    link sharing basics done

--- src/routes/link.api.ts
+++ src/routes/link.api.ts
@@ -1,22 +1,51 @@
 import express from 'express';
-import { Link, ILink } from '../models/link.model';
+import { Link } from '../models/link.model';
+import { User } from '../models/user.model';
 import errorHandler from './error';
 import auth from '../middleware/auth';
 
 const router = express.Router();
 
 // send new link
-router.post('/create', (req, res) => {
+router.post('/create', auth, async (req, res) => {
   const { linkUrl } = req.body;
-  const { recipientId } = req.body;
-  const { senderId } = req.body;
+  const { recipientEmail } = req.body;
+  const { userId: senderId } = req;
 
-  res.status(200).json({ success: true });
+  const recipient = await User.findOne({ email: recipientEmail });
+  if (!recipient) return errorHandler(res, 'Recipient does not exist.');
+  const recipientId = recipient._id;
+  if (recipientId === senderId)
+    return errorHandler(res, 'You cannot send links to yourself.');
+
+  const newLink = new Link({
+    linkUrl,
+    recipientId,
+    senderId,
+  });
+
+  return newLink
+    .save()
+    .then(() => {
+      // TODO: notify recipient here
+
+      return res.status(200).json({ success: true, message: 'New link sent.' });
+    })
+    .catch((err) => {
+      return errorHandler(res, err.message);
+    });
 });
 
-// get all links sent from friends
-router.get('/', (req, res) => {
-  res.status(200).json({ success: true });
+// get all links sent to me
+// TODO: paginate
+// pagination tutorial:
+// https://softwareontheroad.com/pagination-in-nodejs-mongo/
+router.get('/me', auth, async (req, res) => {
+  const { userId } = req;
+
+  const userLinks = await Link.find({ recipientId: userId });
+
+  res.status(200).json({ success: true, data: userLinks });
 });
 
 // TESTING ROUTES BELOW
