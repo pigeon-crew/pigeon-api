786ea9e76d8de17f6a5d98a0ccf2f826307381b2
Daniel Tian <dtian.95@gmail.com>
2020-07-22 19:33:53 +0800

    refactor

--- src/index.ts
+++ src/index.ts
@@ -22,14 +22,6 @@ app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-if (process.env.NODE_ENV === 'production') {
-  const root = path.join(__dirname, 'client', 'build');
-
-  app.use(express.static(root));
-  app.get('/', (_, res) => {
-    res.sendFile('index.html', { root });
-  });
-}
 
 // API Routes
 app.use('/api/users', userRouter);
@@ -37,9 +29,18 @@ app.use('/api/friends', friendReqRouter);
 app.use('/api/links', linkRouter);
 app.use('/api/email', emailRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  const root = path.join(__dirname, 'client', 'build');
+
+  app.use(express.static(root));
+  app.get('*', (_, res) => {
+    res.sendFile('index.html', { root });
+  });
+}
+
 const server = app.listen(app.get('port'), () => {
   console.log(`Listening on port ${app.get('port')} 🚀`);
-  console.log('  Press Command C to stop\n');
+  console.log('  Press Ctrl C to stop\n');
 });
 
 const io = socket(server);
