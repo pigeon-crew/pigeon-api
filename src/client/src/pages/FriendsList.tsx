32c79d1e7f5047c793c01f6f40b589cfcce44ae9
LeytonHo <leyton.ho@gmail.com>
2020-08-01 16:39:37 -0700

    Copy add friends into FriendsList

--- src/client/src/pages/FriendsList.tsx
+++ src/client/src/pages/FriendsList.tsx
@@ -5,6 +5,12 @@ import Dashboard from '../components/layouts/Dashboard';
 import * as API from '../api/auth-api';
 import { ENDPOINT } from '../utils/config';
 import axios from 'axios';
+import { Formik } from 'formik';
+
+interface FormValues {
+  name: string;
+  email: string;
+}
 
 const Container = styled.div`
   display: flex;
@@ -64,9 +70,58 @@ const Option = styled.img`
 `;
 
 const AddContainer = styled.div`
-  box-shadow: 0px 2px 40px 0px rgba(0, 0, 0, 0.7);
+  /* box-shadow: 0px 2px 40px 0px rgba(0, 0, 0, 0.7); */
   border-radius: 20px;
-  margin: 30px auto;
+  margin: 40px auto 0px auto;
+`;
+
+const AddTitle = styled.div`
+  min-height: 1rem;
+  font-size: 2rem;
+  font-weight: 700;
+  text-align: center;
+`;
+
+const InputField = styled.input`
+  width: 200px;
+  color: black;
+  background-color: #f5f6f8;
+  padding: 7px 16px;
+  font-family: 'Avenir';
+  font-weight: 400;
+  font-size: 14px;
+  border: 3px solid #f5f6f8;
+  margin-top: 20px;
+
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.4);
+  }
+
+  &:focus {
+    outline: none;
+    background: white;
+    border: 3px solid #ff8686;
+    color: black;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px auto 10px auto;
+`;
+
+const ErrorText = styled.p`
+  font-family: 'Avenir';
+  font-weight: 500;
+  font-size: 14px;
+  color: #8b0000;
+  margin: 5px auto 0 auto;
 `;
 
 const Friends = () => {
@@ -102,6 +157,12 @@ const Friends = () => {
         });
     });
   }, []);
+
+  const initialValues: FormValues = {
+    name: '',
+    email: '',
+  };
+
   const [friends, setFriends] = useState(null);
   const placeholderFriends = [
     'Jane Doe',
@@ -116,6 +177,32 @@ const Friends = () => {
     'Alex Trebek',
     'Lebron James',
   ];
+
+  const validateSignUp = (values: FormValues) => {
+    const errors = {} as any;
+    if (!values.email) {
+      errors.email = 'Email address required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    if (!values.name) {
+      errors.name = 'Name is required';
+    }
+
+    return errors;
+  };
+
+  const handleSubmit = (
+    values: FormValues,
+    { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }
+  ) => {
+    const requiredValues = {
+      email: values.email,
+    };
+    /* Add in endpoint call */
+  };
+
   return (
     <Dashboard>
       <Container>
@@ -155,7 +242,115 @@ const Friends = () => {
             </FriendsListContainer>
           </ColumnContainer>
         </ListContainer>
-        <AddContainer>Add friends here</AddContainer>
+        <AddContainer>
+          <AddTitle>Add Friends</AddTitle>
+          <Formik
+            initialValues={initialValues}
+            validate={validateSignUp}
+            onSubmit={handleSubmit}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              resetForm,
+            }: /* and other goodies */
+            {
+              values: any;
+              errors: any;
+              touched: any;
+              handleChange: {
+                (e: React.ChangeEvent<any>): void;
+              };
+              handleBlur: {
+                (e: React.ChangeEvent<any>): void;
+              };
+              handleSubmit: {
+                (e: React.ChangeEvent<any>): void;
+              };
+              isSubmitting: boolean;
+              resetForm: {
+                (): void;
+              };
+            }) => (
+              <>
+                <FormContainer onSubmit={handleSubmit}>
+                  <InputField
+                    type="text"
+                    name="name"
+                    value={values.name}
+                    placeholder="Jane Doe"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.name && touched.name && (
+                    <ErrorText>{errors.name}</ErrorText>
+                  )}
+                  <InputField
+                    type="text"
+                    name="email"
+                    value={values.email}
+                    placeholder="janedoe@gmail.com"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.email && touched.email && (
+                    <ErrorText>{errors.email}</ErrorText>
+                  )}
+
+                  <ButtonContainer>
+                    <button
+                      type="submit"
+                      className="ui primary button"
+                      style={{
+                        margin: '15px auto 0 auto',
+                        backgroundColor: 'green',
+                      }}
+                      onClick={(e) => {
+                        handleSubmit(e);
+                      }}
+                    >
+                      Save
+                    </button>
+                  </ButtonContainer>
+                  <ButtonContainer>
+                    <button
+                      type="submit"
+                      className="ui primary button"
+                      style={{
+                        margin: '15px auto 0 auto',
+                        backgroundColor: 'rgba(72,72,72,0.7)',
+                      }}
+                      onClick={() => {
+                        resetForm();
+                      }}
+                    >
+                      Add another friend!
+                    </button>
+                  </ButtonContainer>
+                  <ButtonContainer
+                    style={{
+                      marginTop: '30px',
+                    }}
+                  >
+                    <button
+                      type="submit"
+                      className="ui primary button"
+                      style={{ margin: '15px auto 0 auto' }}
+                      onClick={() => {}}
+                    >
+                      Continue
+                    </button>
+                  </ButtonContainer>
+                </FormContainer>
+              </>
+            )}
+          </Formik>
+        </AddContainer>
       </Container>
     </Dashboard>
   );
