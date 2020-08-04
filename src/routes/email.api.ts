2330bc54d27ca135c23242b6582f6dfff214faf6
LeytonHo <leyton.ho@gmail.com>
2020-08-03 21:36:05 -0700

    Integrate invite friend email

--- src/routes/email.api.ts
+++ src/routes/email.api.ts
@@ -5,7 +5,7 @@ import { SENDGRID_EMAIL } from '../utils/config';
 
 const router = express.Router();
 
-// send email
+// send signup email
 router.post('/signup', async (req, res) => {
   const { email, name } = req.body;
 
@@ -13,13 +13,32 @@ router.post('/signup', async (req, res) => {
     templateName: 'd-eb590ef4bbe4415eb4afd675b0b03dbc',
     sender: SENDGRID_EMAIL,
     receiver: email,
-    template_id: 'd-eb590ef4bbe4415eb4afd675b0b03dbc',
     name,
   };
 
   try {
     await sendEmailTemplate(payload);
     console.log('Signup email sent!');
+    return res.status(200).json({ success: true, message: 'Email sent.' });
+  } catch (error) {
+    return errorHandler(res, error.message);
+  }
+});
+
+// send invite email
+router.post('/invite', async (req, res) => {
+  const { email, name } = req.body;
+
+  const payload = {
+    templateName: 'd-a545cbca1f9b4ff5b30a26aa77e7ae3e',
+    sender: SENDGRID_EMAIL,
+    receiver: email,
+    name,
+  };
+
+  try {
+    await sendEmailTemplate(payload);
+    console.log('Invite email sent!');
     return res.status(200).json({ success: true, message: 'Email sent.' });
   } catch (error) {
     return errorHandler(res, error.message);
